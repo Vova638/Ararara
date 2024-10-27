9a75d06db05ed528d5c05cd81baa4d57cafeb209
Vova638 <v386830@gmail.com>
2024-10-27 13:05:31 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,57 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Bengal cat hub", "RJTheme3")
+
+local Tab = Window:NewTab("main")
+
+local Section = Tab:NewSection("name section")
+
+
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
+
+
+Section:NewSlider("SpeedWalk", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewSlider("SliderText", "Sliderinfo", 200, 16, function(x) 
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = x
+end)
+
+Section:NewSlider("Gravity", "Sliderinfo", 200, 30, function(y) 
+game.Workspace.Gravity = y
+end)
+
+
+Section:NewButton("esp(leave for disable)", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP'))()
+end)
+
+
+Section:NewButton("fly mobile", "ButtonInfo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+end)
+
+Section:NewButton("infinity yield", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
